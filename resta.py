0f471a3b2904cde10decc811cfb34462a0d69871
reggae93 <35476328+reggae93@users.noreply.github.com>
2020-05-21 23:53:34 -0300

    Add files via upload

---
+++ resta.py
@@ -0,0 +1,10 @@
+print("Ingreso De dos Numeros y su Resta")
+n1=int(input("Ingrese el Primer Numero: "))
+n2=int(input("Ingrese el Segundo Numero: "))
+
+resta = n1-n2;
+
+print("La resta es : " + str(resta))
+
+print("Felicidades Resto los dos numeros correctamente :D")
+
